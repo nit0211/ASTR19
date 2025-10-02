b429db60a3522335505f3c4056c657dd5bd79d3b
nit <82301231+nit0211@users.noreply.github.com>
2025-10-02 12:15:42 -0700

    Add files via upload

---
+++ CodePrompt#1.py
@@ -0,0 +1,7 @@
+def PrintHelloWorld():
+    print("My name is Niteesh Eluri")
+    print(" My Pronouns are he/him")
+    print("My Favorite movie is the F1 movie")
+    print("My favorite food is barbecued chicken")
+
+PrintHelloWorld()
