01edfd7cbd3f3e5d26f7b0955db8a38b281a68db
nit <82301231+nit0211@users.noreply.github.com>
2025-10-11 16:36:52 -0700

    Add files via upload

---
+++ CodeJournal3.py
@@ -0,0 +1,15 @@
+def f(x):
+    return x**3+8
+
+def main():
+    x = 9
+    result=f(x)
+
+    print(f"f({x}) gives the result of {result}")
+
+    if result>27:
+     print("YAY")
+
+if __name__ == "__main__":
+  main()
+
